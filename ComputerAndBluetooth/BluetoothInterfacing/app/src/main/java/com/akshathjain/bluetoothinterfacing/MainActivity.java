c90de7ee2ac0d7072341c1379efafbc5664efae7
Akshath Jain <akshath.r.jain@gmail.com>
2017-03-25 12:23:21 -0400

    changes

--- ComputerAndBluetooth/BluetoothInterfacing/app/src/main/java/com/akshathjain/bluetoothinterfacing/MainActivity.java
+++ ComputerAndBluetooth/BluetoothInterfacing/app/src/main/java/com/akshathjain/bluetoothinterfacing/MainActivity.java
@@ -1,17 +1,68 @@
 package com.akshathjain.bluetoothinterfacing;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView output;
+    private Button connectBluetooth;
+    private BluetoothAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView output = (TextView) findViewById(R.id.outputView);
+        output = (TextView) findViewById(R.id.outputView);
+        connectBluetooth = (Button) findViewById(R.id.connectBluetooth);
+        adapter = BluetoothAdapter.getDefaultAdapter();
+
+        //connect to bluetooth when the button is clicked
+        connectBluetooth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(adapter !=  null){
+                    if(!adapter.isEnabled()){
+                        Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(enableBT, 1);
+                        //go to onActivity Result callback
+                    }
+                }
+            }
+        });
 
     }
+
+    //result for trying to get bluetooth access
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode != RESULT_OK){
+            Toast.makeText(this, "Bluetooth could not connect", Toast.LENGTH_SHORT).show();
+        }else{
+            adapter.startDiscovery();
+        }
+    }
+
+    //broadcast receiver for bluetooth discovery
+    private final BroadcastReceiver discoveryReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
 }
