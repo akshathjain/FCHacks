c1701332c45c84570a6f83892b469806d58776fd
NikhilBehari <Nikhil@Nikhils-MacBook-Pro.local>
2017-03-25 12:13:29 -0400

    Some stuff

---
+++ Arduino Interfacing/ArduinoAnalogRead/ArduinoAnalogRead.ino
@@ -0,0 +1,37 @@
+int analogPin1 = 1;     // potentiometer wiper (middle terminal) connected to analog pin 3
+int analogPin4 = 4;     // potentiometer wiper (middle terminal) connected to analog pin 3
+
+                       // outside leads to ground and +5V
+
+int val1 = 0;           // variable to store the value read
+int val4 = 0;           // variable to store the value read
+
+
+
+void setup()
+
+{
+
+  Serial.begin(9600);          //  setup serial
+
+}
+
+
+
+void loop()
+
+{
+
+  val1 = analogRead(analogPin1);
+  val4 = analogRead(analogPin4);
+
+  float voltage1 = val1 * (5.0 / 1023.0);// read the input pin and convert to voltage
+  float voltage4 = val4 * (5.0 / 1023.0);// read the input pin and convert to voltage
+
+  Serial.print("Analog 1 Voltage: ");             // debug value
+  Serial.println(voltage1); 
+  Serial.print("Analog 4 Voltage: ");             // debug value
+  Serial.println(voltage4);             // debug value
+
+}
+
